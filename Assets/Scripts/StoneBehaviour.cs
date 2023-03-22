efba1a56dba509f1a995a1008cfae6e860e4ba14
Merseong <esc990720@korea.ac.kr>
2023-03-22 23:42:51 +0900

    Add aftersync

--- Assets/Scripts/StoneBehaviour.cs
+++ Assets/Scripts/StoneBehaviour.cs
@@ -6,12 +6,30 @@ using Unity.Netcode;
 
 public class StoneBehaviour : NetworkBehaviour
 {
+    Rigidbody Rigidbody;
+    bool moved = false;
 
+    private void Awake()
+    {
+        Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (!moved) return;
+
+        if (IsHost && Rigidbody.velocity == Vector3.zero)
+        {
+            UpdateStoneClientRpc(transform.position);
+            moved = false;
+        }
+    }
 
     [ClientRpc]
     public void AddForceToStoneClientRpc(Vector2 vec, float force)
     {
         GetComponent<Rigidbody2D>().AddForce(vec * force);
+        moved = true;
     }
 
     [ClientRpc]
@@ -20,4 +38,9 @@ public class StoneBehaviour : NetworkBehaviour
         GetComponent<Rigidbody2D>().velocity = Vector2.zero;    
     }
 
+    [ClientRpc]
+    public void UpdateStoneClientRpc(Vector3 position)
+    {
+        transform.position = position;
+    }
 }
